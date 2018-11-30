bc17eef04ad6035f1417b8d2d8f81b409ee69f38
Dan Jackson <dan.jackson@crunch.co.uk>
2018-11-30 11:35:26 +0000

    url prop added to Button component to allow a link to render if url is passed as prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-const Button = ({ type = 'button' }) => {
+const Button = ({ type = 'button', url = '' }) => {
 	
 	const renderButton = () =>
 		<button type={type}>Button</button>
 
+	const isAnchorElement = url && (url.includes('http') || url.startsWith('mailto') || url.startsWith('/'))
+
+	const renderAnchor = () =>
+		<a href={url}>Button</a>
+
 	return (
-		renderButton()
+		isAnchorElement ? renderAnchor() : renderButton()
 	)
 }
 
