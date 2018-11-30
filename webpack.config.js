b1703b70fc27f1016edbd8d7b4aac57a4340437e
Dan Jackson <dan.jackson@crunch.co.uk>
2018-11-30 13:58:24 +0000

    Webpack loaders added to handle CSS and SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: { main: './src/index.js' },
@@ -12,9 +14,28 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: "babel-loader"
+					loader: 'babel-loader'
 				}
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
 			}
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: 'style.css'
+		}),
+		new HtmlWebpackPlugin({
+			inject: false,
+			hash: true,
+			template: './src/index.html',
+			filename: 'index.html'
+		})
+	]
 };
