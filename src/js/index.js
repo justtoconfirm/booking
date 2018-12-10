35c7c57207695d5220fabe94826358dfc4a8a9a6
Dan Jackson <dan.jackson@crunch.co.uk>
2018-12-10 08:16:43 +0000

    React Router implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import App from './components/App'
+import Users from './components/Users'
+import Contact from './components/Contact'
 
-import Button from './components/Button'
+const routing = (
+	<Router>
+		<div>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/users">Users</Link>
+				</li>
+				<li>
+					<Link to="/contact">Contact</Link>
+				</li>
+			</ul>
+			<Route exact path="/" component={App} />
+			<Route path="/users" component={Users} />
+			<Route path="/contact" component={Contact} />
+		</div>
+	</Router>
+)
 
 ReactDOM.render(
-	<Button />,
+	routing,
 	document.getElementById('app')
 )
