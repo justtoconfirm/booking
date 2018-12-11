6f79054229b0b80b2d9865b33ea2ba7a335c2f29
Dan Jackson <dan.jackson@crunch.co.uk>
2018-12-11 12:07:36 +0000

    Atomic design methodology applied to JS file structure

--- src/js/components/5-pages/Users.js
+++ src/js/components/5-pages/Users.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Button from './Button'
 
 class Users extends React.Component {
   render() {
     return (
     	<div>
     		<h1>Users</h1>
-    		<Button theme="primary">Test</Button>
     	</div>
     )
   }
--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
-import App from './components/App'
-import Users from './components/Users'
-import Contact from './components/Contact'
-import Notfound from './components/Notfound'
+import App from './components/5-pages/App'
+import Users from './components/5-pages/Users'
+import Contact from './components/5-pages/Contact'
+import Notfound from './components/5-pages/Notfound'
 
 const routing = (
 	<Router>
 		<div>
 			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/users">Users</Link>
-				</li>
-				<li>
-					<Link to="/contact">Contact</Link>
-				</li>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/users">Users</Link></li>
+				<li><Link to="/contact">Contact</Link></li>
 			</ul>
 			<Switch>
 				<Route exact path="/" component={App} />
