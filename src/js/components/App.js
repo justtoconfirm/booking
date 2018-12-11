da5f0fe472c9a322f254c32b999d2aa75f25e7fe
Dan Jackson <dan.jackson@crunch.co.uk>
2018-12-11 13:35:29 +0000

    Removal of Link import

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Navigation from './2-molecules/navigation/Navigation'
 import Home from './5-pages/Home'
 import Users from './5-pages/Users'
