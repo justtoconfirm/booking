a989e88240821287e4e4b17a92bd6fc6a0006921
Dan Jackson <dan.jackson@crunch.co.uk>
2018-12-11 13:53:50 +0000

    Footer component added

---
+++ src/js/components/3-organisms/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = () => {
+	return (
+		<footer>
+			<div>Footer</div>
+		</footer>
+	)
+}
+
+export default Header
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,6 +6,7 @@ import Home from './5-pages/Home'
 import Users from './5-pages/Users'
 import Contact from './5-pages/Contact'
 import Notfound from './5-pages/Notfound'
+import Footer from './3-organisms/footer/Footer'
 
 const App = () => {
 	return (
@@ -18,6 +19,7 @@ const App = () => {
 					<Route path="/contact" component={Contact} />
 					<Route component={Notfound} />
 				</Switch>
+				<Footer />
 			</React.Fragment>
 		</Router>
 	)
