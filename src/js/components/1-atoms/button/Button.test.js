3137526e353ccf5c348dc0449b822f2aa366c397
Dan Jackson <dan.jackson@crunch.co.uk>
2018-12-17 12:45:21 +0000

    Snapshot created for Button component using Jest

--- src/js/components/1-atoms/button/Button.test.js
+++ src/js/components/1-atoms/button/Button.test.js
@@ -3,7 +3,11 @@ import { shallow } from 'enzyme'
 import Button from './Button'
 
 describe('<Button/>', () => {
-	it('should render without throwing an error', () => {
+	it('Button renders correctly', () => {
+		expect(shallow(<Button/>)).toMatchSnapshot()
+	})
+
+	it('Button should render without throwing an error', () => {
 		expect(shallow(<Button/>).find('.btn').exists()).toBe(true)
 	})
 })
